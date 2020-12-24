7e645bcbfa1477a4308bab4a73307b0eb886f1d7
Mateus Macedo <mateusouza2014@live.com>
2020-12-24 00:35:30 -0300

    nova versão

---
+++ automacao-api/src/main/java/com/projeto/api/automacao/AutomacaoApiApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.api.automacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomacaoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomacaoApiApplication.class, args);
+	}
+
+}
---
+++ automacao-api/src/test/java/com/projeto/api/automacao/AutomacaoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.api.automacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomacaoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
